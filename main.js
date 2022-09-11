2d8b8aa8039cdc8a689f84e0d52c360ca2d9ec84
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:24:19 +0530

    unshift

---
+++ main.js
@@ -0,0 +1,7 @@
+let subject=["Maths","Science","Social"];
+console.log("original array :", subject);
+console.log("length of the array is "+subject.length);
+console.log("method : array.unshift(Tamil,English)");
+subject.unshift("Tamil","English");
+console.log("change in array :",subject);
+console.log("return: length of array after unshift() is "+subject.length);
